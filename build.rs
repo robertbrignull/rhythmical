0d9a3ee3a618d9df654dd401971e21c53e18f42a
Robert Brignull <robertbrignull@gmail.com>
2018-10-22 17:57:43 +0100

    remove debug commands

--- build.rs
+++ build.rs
@@ -5,7 +5,6 @@ use std::process::Command;
 fn main() {
     let current_dir = env::current_dir().unwrap();
     let out_dir = env::var("OUT_DIR").unwrap();
-    println!("{:?},  {}", current_dir, out_dir);
 
     Command::new("tsc")
         .arg("--project")
