bd57c6bdc62e3652e508cfcc162c84b0db82b113
Robert Brignull <robertbrignull@gmail.com>
2018-10-22 17:56:56 +0100

    Serve app.js file

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,23 @@
 extern crate rocket;
 
+use std::fs::File;
+use std::io::Read;
+
 #[get("/")]
 fn root() -> String {
     return format!("Hello, world!")
 }
 
+#[get("/app.js")]
+fn app_js() -> String {
+    let mut out_js = File::open(concat!(env!("OUT_DIR"), "/app.js")).unwrap();
+    let mut out_js_contents = String::new();
+    out_js.read_to_string(&mut out_js_contents).unwrap();
+    return out_js_contents;
+}
+
 pub fn start_server() {
-    rocket::ignite().mount("/", routes![root]).launch();
+    rocket::ignite()
+        .mount("/", routes![root, app_js])
+        .launch();
 }
