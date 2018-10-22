e12326452c69cd6d8fe26435ae766e95293fe68d
Robert Brignull <robertbrignull@gmail.com>
2018-10-22 16:56:28 +0100

    add basic react app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        Hello, world!
+      </div>
+    );
+  }
+}
+
+export default App;
