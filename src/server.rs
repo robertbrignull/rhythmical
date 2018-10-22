2d88705812bedb5d4995540162df889ae8abc2b2
Robert Brignull <robertbrignull@gmail.com>
2018-10-22 18:13:38 +0100

    serve index.html

--- src/server.rs
+++ src/server.rs
@@ -1,19 +1,26 @@
 extern crate rocket;
 
+use std::env;
 use std::fs::File;
 use std::io::Read;
+use rocket::{response::content};
 
 #[get("/")]
-fn root() -> String {
-    return format!("Hello, world!")
+fn root() -> content::Html<String> {
+    let current_dir = env::current_dir().unwrap();
+    let index_html_path = current_dir.join("frontend/public/index.html");
+    let mut index_html = File::open(index_html_path).unwrap();
+    let mut index_html_contents = String::new();
+    index_html.read_to_string(&mut index_html_contents).unwrap();
+    return content::Html(index_html_contents);
 }
 
 #[get("/app.js")]
-fn app_js() -> String {
+fn app_js() -> content::JavaScript<String> {
     let mut out_js = File::open(concat!(env!("OUT_DIR"), "/app.js")).unwrap();
     let mut out_js_contents = String::new();
     out_js.read_to_string(&mut out_js_contents).unwrap();
-    return out_js_contents;
+    return content::JavaScript(out_js_contents);
 }
 
 pub fn start_server() {
