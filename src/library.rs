5cf86405c7d60331ad56c0267c3ae1883f74de5f
Robert Brignull <robertbrignull@gmail.com>
2018-10-28 11:34:24 +0000

    parse library from json file

--- src/library.rs
+++ src/library.rs
@@ -1,38 +1,28 @@
-use std::collections::HashMap;
+extern crate serde_json;
 
-#[derive(PartialOrd, PartialEq, Ord, Eq, Hash, Clone)]
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
+
+#[derive(PartialOrd, PartialEq, Ord, Eq, Hash, Clone, Serialize, Deserialize)]
 pub struct Song {
     pub id: u32,
     pub name: String,
     pub file_location: String,
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Library {
     pub songs: HashMap<u32, Song>,
 }
 
 pub fn load_library() -> Library {
-    let mut songs: HashMap<u32, Song> = HashMap::new();
+    let mut file = File::open("/home/robert/Documents/coding/mine/rust/rhythmical/library.json").unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
 
-    songs.insert(1, Song {
-        id: 1,
-        name: "Hump de Bump".to_string(),
-        file_location: "/home/robert/Music/Red Hot Chili Peppers/Stadium Arcadium/Hump de Bump.mp3".to_string(),
-    });
-
-    songs.insert(2, Song {
-        id: 2,
-        name: "Ojos Asi".to_string(),
-        file_location: "/home/robert/Music/Facebook-TodoenMP3/Donde Estan Los Ladrones (Russian Edition)/11 - Ojos Asi.mp3".to_string(),
-    });
-
-    songs.insert(3, Song {
-        id: 3,
-        name: "Dont You Worry Bout A Thing".to_string(),
-        file_location: "/home/robert/Music/Stevie Wonder/Live at Last/Dont You Worry Bout A Thing.mp3".to_string(),
-    });
-
-    return Library {
-        songs
-    }
+    return match serde_json::from_str(&data) {
+        Ok(library) => library,
+        Err(error) => panic!("Unable to parse library: {}", error)
+    };
 }
