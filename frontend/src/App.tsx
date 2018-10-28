995c7d0b0f510413c1a7161e544d45f9353fec30
Robert Brignull <robertbrignull@gmail.com>
2018-10-28 08:25:01 +0000

    add an audio player

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
 import './App.css';
 import Api from './api';
+import {RefObject} from "react";
 
 interface AppState {
-  songs?: Song[]
+  songs?: Song[];
+  currentSong?: Song;
 }
 
 class App extends React.Component<{}, AppState> {
+
+  private readonly audio: RefObject<HTMLAudioElement>;
+
   constructor(props: {}) {
     super(props);
 
+    this.audio = React.createRef();
+
     this.state = {
-      songs: undefined
+      songs: undefined,
+      currentSong: undefined,
     };
   }
 
@@ -21,12 +29,36 @@ class App extends React.Component<{}, AppState> {
     });
   }
 
+  private playSong(song: Song) {
+    this.setState(
+      { currentSong: song },
+      () => {
+        if (this.audio.current) {
+          this.audio.current.load();
+          this.audio.current.play()
+            .catch(error => console.error("Unable to play: ", error));
+        }
+      });
+  }
+
+  private static getSongSource(song: Song): string {
+    return "/api/songs/" + song.id + "/contents";
+  }
+
   public render() {
     if (this.state.songs) {
       return (
         <div>
+          <audio controls ref={this.audio}>
+            { this.state.currentSong
+              ? <source src={App.getSongSource(this.state.currentSong)} />
+              : null }
+          </audio>
           {... this.state.songs.map(song =>
             <div key={song.id} className="song">
+              <button onClick={() => this.playSong(song)}>
+                Play
+              </button>
               { song.name }
             </div>
           )}
