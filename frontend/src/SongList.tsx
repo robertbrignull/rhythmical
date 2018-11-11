ecfde220d55d1326c4422c4156f2578f4b6e5995
Robert Brignull <robertbrignull@gmail.com>
2018-11-11 12:11:56 +0000

    allow sorting on any column

--- frontend/src/SongList.tsx
+++ frontend/src/SongList.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
 import Api from "./api";
 
-type SortMode = 'id';
+type SortMode = 'title' | 'genre' | 'artist' | 'album' | 'duration' | 'rating';
+type SortDirection = 'ascending' | 'descending';
 
 interface SongListProps {
   currentSong?: Song;
@@ -13,6 +14,7 @@ interface SongListProps {
 interface SongListState {
   songs?: Song[];
   sortMode: SortMode;
+  sortDirection: SortDirection;
 }
 
 export class SongList extends React.Component<SongListProps, SongListState> {
@@ -21,27 +23,72 @@ export class SongList extends React.Component<SongListProps, SongListState> {
 
     this.state = {
       songs: undefined,
-      sortMode: 'id'
+      sortMode: 'artist',
+      sortDirection: 'ascending',
     };
   }
 
   public componentDidMount() {
     Api.songs.getAll().then((songs: Song[]) => {
-      this.sortSongs(songs);
+      SongList.sortSongs(songs, this.state.sortMode, this.state.sortDirection);
       this.setState({ songs });
     });
   }
 
-  private sortSongs(songs: Song[]) {
-    if (this.state.sortMode === 'id') {
-      songs.sort((a, b) => a.id - b.id);
+  private static sortSongs(songs: Song[], sortMode: SortMode, sortDirection: SortDirection) {
+    let cmp: (a: Song, b: Song) => number;
+    if (sortMode === 'title') {
+      cmp = (a, b) => a.title.localeCompare(b.title);
+    } else if (sortMode === 'genre') {
+      cmp = (a, b) => a.genre.localeCompare(b.genre);
+    } else if (sortMode === 'artist') {
+      cmp = (a, b) => a.artist.localeCompare(b.artist);
+    } else if (sortMode === 'album') {
+      cmp = (a, b) => a.album.localeCompare(b.album);
+    } else if (sortMode === 'duration') {
+      cmp = (a, b) => a.duration - b.duration;
+    } else if (sortMode === 'rating') {
+      cmp = (a, b) => a.rating - b.rating;
+    } else {
+      cmp = () => 0;
     }
+
+    songs.sort(sortDirection === 'ascending'
+      ? cmp : (a: Song, b: Song) => cmp(b, a));
   }
 
   private isPlaying(song: Song): boolean {
     return this.props.playing &&
       this.props.currentSong !== undefined &&
       this.props.currentSong.id === song.id;
+  }
+
+  private renderSortIcon(key: SortMode) {
+    let onClick = () => {
+      this.setState((state) => {
+        let songs = state.songs;
+        let sortMode = key;
+        let sortDirection: SortDirection =
+          state.sortMode === key && state.sortDirection === 'ascending'
+            ? 'descending' : 'ascending';
+        if (songs !== undefined) {
+          SongList.sortSongs(songs, sortMode, sortDirection);
+        }
+        return { songs, sortMode, sortDirection };
+      });
+    };
+
+    if (this.state.sortMode === key) {
+      if (this.state.sortDirection === 'ascending') {
+        return <i className="fa fa-fw fa-sort-up selected-sort"
+                  onClick={onClick}/>;
+      } else {
+        return <i className="fa fa-fw fa-sort-down selected-sort"
+                  onClick={onClick}/>;
+      }
+    }
+    return <i className="fa fa-fw fa-sort"
+              onClick={onClick}/>;
   }
 
   private renderDuration(duration: number) {
@@ -70,12 +117,30 @@ export class SongList extends React.Component<SongListProps, SongListState> {
             <thead>
               <tr>
                 <th/>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Artist</th>
-                <th>Album</th>
-                <th>Duration</th>
-                <th>Rating</th>
+                <th>
+                  Title
+                  { this.renderSortIcon('title') }
+                </th>
+                <th>
+                  Genre
+                  { this.renderSortIcon('genre') }
+                </th>
+                <th>
+                  Artist
+                  { this.renderSortIcon('artist') }
+                </th>
+                <th>
+                  Album
+                  { this.renderSortIcon('album') }
+                </th>
+                <th>
+                  Duration
+                  { this.renderSortIcon('duration') }
+                </th>
+                <th>
+                  Rating
+                  { this.renderSortIcon('rating') }
+                </th>
               </tr>
             </thead>
           </table>
