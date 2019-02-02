a81161aa8337b7bf62f0a27a348918cb264f1ec0
Robert Brignull <robertbrignull@gmail.com>
2019-02-02 17:40:23 +0000

    add error checking to gsutil commands

--- src/gsutil.rs
+++ src/gsutil.rs
@@ -7,7 +7,7 @@ pub fn cat(path: &String) -> Vec<u8> {
     let mut cmd = Command::new("gsutil");
     cmd.arg("cat");
     cmd.arg(format!("gs://{}{}", project_name, path));
-    return cmd.output().unwrap().stdout;
+    return execute(cmd);
 }
 
 pub fn sign(path: &String) -> String {
@@ -18,10 +18,20 @@ pub fn sign(path: &String) -> String {
         .arg(&args.private_key)
         .arg(format!("gs://{}{}", args.project_name, path));
 
-    let output = String::from_utf8(cmd.output().unwrap().stdout).unwrap();
+    let output = String::from_utf8(execute(cmd)).unwrap();
 
     return match output.find("https://storage.googleapis.com") {
         Some(i) => output.get(i..).unwrap().to_string(),
         None => panic!(format!("gsutil signurl output did not contain url: {}", output))
     };
+}
+
+fn execute(mut cmd: Command) -> Vec<u8> {
+    let output = cmd.output().unwrap();
+    if !output.status.success() {
+        println!("stdout:\n{}", String::from_utf8(output.stdout).unwrap());
+        println!("stderr:\n{}", String::from_utf8(output.stderr).unwrap());
+        panic!("gsutil outputted {}", output.status.code().unwrap_or(-1));
+    }
+    return output.stdout;
 }
