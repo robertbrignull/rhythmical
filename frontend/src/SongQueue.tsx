7954fc2dc28de7a7c23f4975b932698a942e0d8e
Robert Brignull <robertbrignull@gmail.com>
2022-07-24 21:55:14 +0100

    Add styling of song queue

--- frontend/src/SongQueue.tsx
+++ frontend/src/SongQueue.tsx
@@ -10,18 +10,39 @@ interface SongQueueState {
   songIdQueue: string[];
 }
 
+const INTERNAL_QUEUE_LENGTH = 50;
+const VISIBLE_QUEUE_LENGTH = 5;
+
 export class SongQueue extends React.PureComponent<SongQueueProps, SongQueueState> {
   constructor(props: SongQueueProps) {
     super(props);
 
+    const songIdQueue: string[] = [];
+    for (let i = 0; i < INTERNAL_QUEUE_LENGTH; i++) {
+      songIdQueue.push(props.songIds[Math.floor(Math.random() * props.songIds.length)]);
+    }
+
     this.state = {
-      songIdQueue: [],
+      songIdQueue: songIdQueue,
     };
   }
   
   public render() {
     return <div className="song-queue">
-      <span>Song queue!</span>
+      {
+        this.state.songIdQueue.slice(0, VISIBLE_QUEUE_LENGTH).map(songId => {
+          const song = this.props.library.getSong(songId);
+          if (song) {
+            return <div className="song-queue-item">
+              <div className="title">{ song.title }</div>
+              <div className="artist">by { song.artist }</div>
+              <div className="album">from { song.album }</div>
+            </div>
+          } else {
+            return undefined;
+          }
+        })
+      }
     </div>;
   }
 }
