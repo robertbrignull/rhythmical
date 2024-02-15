91ae188977ff7eea4a15c08b1c20cb0e23946065
Robert <robertbrignull@github.com>
2024-02-15 12:42:26 +0000

    Install eslint-plugin-react-hooks

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended"
     ],
     "overrides": [
         {
